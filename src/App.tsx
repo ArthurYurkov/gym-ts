2573aff10b9622830c4d81394b283581be97ecde
ArthurYurkov <yurkovarthur@gmail.com>
2024-09-19 12:31:42 +0300

    Add mobile menu, ts enum, open/close modal btn, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import Navbar from '@/scenes/Navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 
 export default function App() {
     const [selectedPage, setSelectedPage] = useState<SelectedPage>(
         SelectedPage.Home,
     );
+    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY === 0) {
+                setIsTopOfPage(true);
+                setSelectedPage(SelectedPage.Home);
+            }
+            if (window.scrollY !== 0) {
+                setIsTopOfPage(false);
+            }
+            window.addEventListener('scroll', handleScroll);
+            return () => window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className="app bg-gray-20">
             <Navbar
+                isTopOfPage={isTopOfPage}
                 selectedPage={selectedPage}
                 setSelectedPage={setSelectedPage}
             />
