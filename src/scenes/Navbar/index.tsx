f6ff008186bb41dc520c1a8ee75d9cb191791fa2
ArthurYurkov <yurkovarthur@gmail.com>
2024-09-19 11:42:40 +0300

    Add "Action Button", made mobile menu with open/close buttons

--- src/scenes/Navbar/index.tsx
+++ src/scenes/Navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from '@/assets/Logo.png';
 import Link from './Link';
 import { SelectedPage } from '@/shared/types';
 import useMediaQuery from '@/hooks/useMediaQuery';
+import ActionButton from '@/shared/ActionButton';
 
 type Props = {
     selectedPage: SelectedPage;
@@ -12,6 +13,7 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     const flexBetween = 'flex items-center justify-between';
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
     const isAboveMediaScreen = useMediaQuery('(min-width:1060px)');
 
     return (
@@ -49,15 +51,63 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                                 </div>
                                 <div className={`${flexBetween} gap-8`}>
                                     <p>Sign In</p>
-                                    <button>Become a member</button>
+                                    <ActionButton
+                                        setSelectedPage={setSelectedPage}
+                                    >
+                                        Become a member
+                                    </ActionButton>
                                 </div>
                             </div>
                         ) : (
-                            ''
+                            <button
+                                className="rounded-full bg-secondary-500 p-2"
+                                onClick={() => setIsMenuToggled(!isMenuToggled)}
+                            >
+                                <Bars3Icon className="text h-6 w-6 text-white" />
+                            </button>
                         )}
                     </div>
                 </div>
             </div>
+            {/* MOBILE MENU MODAL*/}
+            {!isAboveMediaScreen && isMenuToggled && (
+                <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+                    {/* CLOSE ICON */}
+                    <div className="flex justify-end p-12">
+                        <button
+                            onClick={() => setIsMenuToggled(!isMenuToggled)}
+                        >
+                            <XMarkIcon className="h-6 w-6 text-gray-400" />
+                        </button>
+                    </div>
+                    {/* MENU ITEMS */}
+                    <div className={`ml-[33%] flex flex-col gap-10 text-2xl`}>
+                        <Link
+                            page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+
+                        <Link
+                            page="Benefits"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+
+                        <Link
+                            page="Our Classes"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+
+                        <Link
+                            page="Contact Us"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    </div>
+                </div>
+            )}
         </nav>
     );
 };
